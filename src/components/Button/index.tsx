d72853ae8ba8ce52de810f3fb405180f4f03f493
Rafael Santiago <rafaelsantiagods@gmail.com>
2020-11-02 20:49:03 -0300

    fix(Button): add prop onClick

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,10 +4,13 @@ import { Container } from './button.styles'
 interface IProps {
   text: string
   isRed?: boolean
+  onClick?(event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void
 }
 
-const Button: React.FC<IProps> = ({ text, isRed }) => (
-  <Container isRed={isRed}>{text}</Container>
+const Button: React.FC<IProps> = ({ text, isRed, onClick }) => (
+  <Container isRed={isRed} onClick={onClick}>
+    {text}
+  </Container>
 )
 
 export default Button
